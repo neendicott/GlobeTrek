dfe5234aca2770161e05b60dff617bcbf99d67b8
Nate Endicott <Nateendicott28@gmail.com>
2023-08-10 23:02:57 -0400

    changed files and folders around as well as added to app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 #Defined a route for the root URL using the decorator. This function below is called when someone visits the root URL
 @app.route('/')
 def index(): 
