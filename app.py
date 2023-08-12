680f5b4007e178e585b7af779ba7530d5d50ac23
Nate Endicott <Nateendicott28@gmail.com>
2023-08-11 23:58:22 -0400

    Once you click Profile, got it to load a profile!

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='/static')
-#Defined a route for the root URL using the decorator. This function below is called when someone visits the root URL
+
 @app.route('/')
 def index(): 
     return render_template('index.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
