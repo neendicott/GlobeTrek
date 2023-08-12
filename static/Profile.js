585f2e9960153362ce464a4922c33b95d2b44dc3
Nate Endicott <Nateendicott28@gmail.com>
2023-08-11 23:47:04 -0400

    Added profile HTML so you go to a seperate page

--- static/Profile.js
+++ static/Profile.js
@@ -1,18 +1,33 @@
-// profile.js
-document.addEventListener('DOMContentLoaded', function () {
-    const profileModal = document.getElementById('profileModal');
-    const profileLink = document.querySelector('.nav__link.profile');
+// Get references to the modal and its content
+const profileModal = document.getElementById('profileModal');
+const modalContent = profileModal.querySelector('.modal-content');
 
-    // Open modal when clicking on "Profile" link
-    profileLink.addEventListener('click', function (event) {
-        event.preventDefault();
-        profileModal.style.display = 'block';
-    });
+// Get references to the button or element that opens the modal
+const openProfileButton = document.getElementById('openProfileButton');
 
-    // Close modal when clicking outside of it
-    window.addEventListener('click', function (event) {
-        if (event.target === profileModal) {
-            profileModal.style.display = 'none';
-        }
-    });
+// Function to open the modal
+function openModal() {
+    profileModal.style.display = 'block';
+}
+
+// Function to close the modal
+function closeModal() {
+    profileModal.style.display = 'none';
+}
+
+// Add event listener to open the modal
+openProfileButton.addEventListener('click', openModal);
+
+// Add event listener to close the modal when clicking outside the content
+window.addEventListener('click', (event) => {
+    if (event.target === profileModal) {
+        closeModal();
+    }
 });
+
+// Add event listener to close the modal when pressing the Escape key
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        closeModal();
+    }
+
