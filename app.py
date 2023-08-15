3116ce23d5b18266fe3cb3b870a4a53f9b956e8b
Nate Endicott <Nateendicott28@gmail.com>
2023-08-15 02:14:58 -0400

    Successfully added login and signup buttons to work on nav bar and direct you to a seperate page

--- app.py
+++ app.py
@@ -21,6 +21,14 @@ class User(db.Model, UserMixin):
 def index(): 
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 @app.route('/profile')
 def profile():
     user = {
