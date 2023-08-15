bbf338176567178a9e09f4bee616dad34018381e
Nate Endicott <Nateendicott28@gmail.com>
2023-08-14 23:33:25 -0400

    installed Flask-login for user authentication

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__, static_url_path='/static')
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 @app.route('/')
 def index(): 
